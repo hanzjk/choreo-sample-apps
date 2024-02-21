8a1e1c48b07c4112a862c4776eb6f5c1358b3750
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-02-21 11:39:36 +0530

    Update main.go

--- go/greeter/main.go
+++ go/greeter/main.go
@@ -65,11 +65,10 @@ func main() {
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	serviceURL := os.Getenv("SVC_URL")
 
 	name := r.URL.Query().Get("name")
 	if name == "" {
 		name = "Stranger"
 	}
-	fmt.Fprintf(w, "Hello, %s!\n", serviceURL)
+	fmt.Fprintf(w, "Hello, %s!\n", name)
 }
